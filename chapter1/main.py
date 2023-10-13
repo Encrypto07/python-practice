8d494874bc781cfdde455eacb608f2b0a2b39928
Encrypto07 <sultan8512@gmail.com>
2023-10-13 19:41:42 +0530

    first commit

---
+++ chapter1/main.py
@@ -0,0 +1,16 @@
+import os #imported the os module
+
+print("Hello World")
+
+print("twinkle twinkle little star")
+
+
+print("The directory contents = ",os.listdir("/Users/sultansheikh/go/src/github.com/Encrypto07"))
+
+
+# single line comment
+
+'''
+Multi line comments
+are done in this way
+'''
